747dbd9a6b815d8f228931f8bd1438df53e04176
KenJiKun-Q <51771104+KenJiKun-Q@users.noreply.github.com>
2019-11-04 00:33:28 +0800

    全选/不全选/计算价钱

--- pages/car/index.js
+++ pages/car/index.js
@@ -6,7 +6,10 @@ Page({
    */
   data: {
     address:{},
-    goods:null
+    goods:null,
+    //总价格
+    allPrice:0,
+    allSelected:true
   },
 
   /**
@@ -21,10 +24,16 @@ Page({
     let goods = wx.getStorageSync("goods") || null;
 
     this.setData({
-      goods
+      goods,
     })
+
+    this.handlePrice();
+
+    this.handleAllSelected()
   },
 
+
+  //渲染数据
   hanldeAddress(){
     wx.chooseAddress({
       success:(res) => {
@@ -38,5 +47,201 @@ Page({
         })
       }
     })
+  },
+
+
+  // 增加数量1
+  handleAdd(event){
+    let { id } = event.currentTarget.dataset;
+
+    let {goods} = this.data;
+    // 数量增加1
+    goods[id].number += 1;
+    
+    this.setData({
+      goods
+    });
+
+    //保存到本地
+    wx.setStorageSync("goods", goods)
+
+    this.handlePrice()
+
+    this.handleAllSelected()
+  },
+
+
+  // 数量减1
+  handleReduce(event){
+    let { id } = event.currentTarget.dataset;
+
+    let { goods } = this.data;
+
+    // 判断数量小于1的时候
+    if(goods[id].number <= 1){
+      wx.showModal({
+        title: '提示',
+        content: '请问确认删除商品?',
+        success:(res) => {
+          if (res.confirm) {
+            //删除商品
+            delete goods[id];
+            // 由于showModal是异步执行，所以需要把修改data值的方式放到success中
+            this.setData({
+              goods
+            })
+
+            //保存到本地
+            wx.setStorageSync("goods", goods);
+
+            this.handlePrice();
+
+            this.handleAllSelected()
+          }
+        }
+      })
+    }else{
+      //数量减1
+      goods[id].number -= 1;
+
+      this.setData({
+        goods
+      })
+
+       wx.setStorageSync("goods", goods);
+
+      this.handlePrice()
+
+      this.handleAllSelected()
+    }
+  },
+
+
+  // 检测是否有小数点
+  bindInput(event){
+    //获取输入的值
+    let value = +event.detail.value;
+    let {id} = event.target.dataset;
+    let {goods} = this.data;
+
+    //判断是否有小数点,如果有就向下取整
+    goods[id].number = Math.floor(value);
+    
+    // 修改data的值
+    this.setData({
+      goods
+    });
+  },
+
+  // 输入的数量
+  bindChange(event){
+    //获取输入框的值
+    // console.log(event)
+    let value = +event.detail.value
+    let { id } = event.target.dataset;
+    let { goods } = this.data;
+
+    //如果是空的或者是0
+    if(value === 0){
+      goods[id].number = 1;
+    }else{
+      goods[id].number = value
+    }
+
+    // 修改data的值
+    this.setData({
+      goods
+    });
+
+
+    wx.setStorageSync("goods", goods);
+
+    this.handlePrice()
+
+    this.handleAllSelected()
+
+  },
+
+  // 选中状态取反
+  hanldeSelected(event){
+    let {id} = event.target.dataset;
+    let {goods} = this.data;
+
+    //把选中的状态取反
+    goods[id].selected = !goods[id].selected;
+
+    this.setData({
+      goods
+    });
+
+    // 保存到本地
+    wx.setStorageSync("goods", goods);
+
+    this.handlePrice()
+    
+    this.handleAllSelected()
+  },
+
+
+
+  // 小程序没有computed属性,所以需要封装计算总价格的函数
+  handlePrice(){
+    let {goods} = this.data;
+    let price = 0;
+
+   //开始计算,v就是key 也就是商品的id
+   Object.keys(goods).map(v => {
+     //当前商品必须是选中的
+     if(goods[v].selected){
+       //单价乘以数量
+       price += (goods[v].goods_price * goods[v].number)
+     }
+   })
+
+   this.setData({
+     allPrice:price
+   })
+  },
+
+  //全选功能
+  handleAllSelected(){
+    let { goods } = this.data;
+
+    let isSelect = true;
+
+    // 判断是否有一个没有选中
+    Object.keys(goods).forEach(v => {
+      if(!goods[v].selected){
+        isSelect = false;
+      }
+    })
+
+    this.setData({
+      allSelected : isSelect
+    })
+  },
+
+
+
+  handleAllSelectedEvent(){
+    let {goods, allSelected} = this.data
+
+    // 循环取反选中状态，取反是根据allSelected
+    Object.keys(goods).forEach(v => {
+      goods[v].selected = !allSelected
+    })
+
+    this.setData({
+      goods,
+      // 判断全选状态
+      allSelected: !allSelected
+    })
+
+    // 保存到本地
+    wx.setStorageSync("goods", goods);
+
+    this.handlePrice()
   }
 })
+
+
